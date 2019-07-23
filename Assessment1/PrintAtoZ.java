bc1cf49cf210f5118c9a74f522e5bec82f7f4645
AjithaRamesh1 <53213400+AjithaRamesh1@users.noreply.github.com>
2019-07-23 13:45:45 +0530

    Add files via upload

---
+++ Assessment1/PrintAtoZ.java
@@ -0,0 +1,13 @@
+
+public class PrintAtoZ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+for (int i=65;i<=90;i++)
+{ char j=(char)i;
+	System.out.println(j);
+}
+	}
+
+}
